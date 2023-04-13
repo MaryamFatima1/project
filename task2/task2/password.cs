6e970c8cb1a62e6040655b1008c4e7e89b3938dd
MaryamFatima1 <129974130+MaryamFatima1@users.noreply.github.com>
2023-04-13 20:58:25 +0500

    Add files via upload

---
+++ task2/task2/password.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class password
+    {
+        int option;
+
+        static string parseData(string record, int field)
+        {
+            int comma = 1;
+            string item = "";
+            for (int x = 0; x < record.Length; x++)
+            {
+                if (record[x] == ',')
+                {
+                    comma++;
+                }
+                else if (comma == field)
+                {
+                    item = item + record[x];
+                }
+            }
+            return item;
+        }
+
+        static void readData(string path, string[] names, string[] password)
+        {
+            int x = 0;
+            if (File.Exists(path))
+            {
+                StreamReader fileVariable = new StreamReader(path);
+                string record;
+                while ((record = fileVariable.ReadLine()) != null)
+                {
+                    names[x] = parseData(record, 1);
+                    password[x] = parseData(record, 2);
+                    x++;
+                    if (x >= 5)
+                    {
+                        break;
+                    }
+                }
+                fileVariable.Close();
+            }
+            else
+            {
+                Console.WriteLine("not exist");
+            }
+        }
+
+        static void signIn(string n, string p, string[] names, string[] password)
+        {
+            bool flag = false;
+            for (int x = 0; x < 5; x++)
+            {
+                if (n == names[x] && p == password[x])
+                {
+                    Console.WriteLine("valid user");
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("invalid user");
+            }
+            Console.ReadKey();
+        }
+
+        static void signUp(string path, string n, string p)
+        {
+            StreamWriter file = new StreamWriter(path, true);
+            file.WriteLine(n + "," + p);
+            file.Flush();
+            file.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            int option;
+            Console.WriteLine("1.signIn");
+            Console.WriteLine("2.signUp");
+            Console.WriteLine("Enter Option ");
+            option = int.Parse(Console.ReadLine());
+        }
+    }
+}
